5ee8c9e1935559073bedd32e870ebaa4193b6e2a
paulkahura <pablokahura@gmail.com>
2020-01-02 13:58:18 +0300

    fixed flake formatting

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,9 +6,9 @@ class ModelTests(TestCase):
 
     def test_create_user_with_eamil_successful(self):
         """Test creatinf a new user with an email is successful"""
-        email='test@gmail.com'
-        password='Testpass123'
-        user=get_user_model().objects.create_user(
+        email = 'test@gmail.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
@@ -19,7 +19,7 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = 'parelli@gmail.com'
-        user =  get_user_model().objects.create_user(email, 'test123')
+        user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
 
